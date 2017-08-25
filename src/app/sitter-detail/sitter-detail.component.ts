c2a9940fed2dbde6ed6967c00ba6db6edfe29bd1
Laura Hamilton <laurahamilton9@gmail.com>
2017-08-25 15:05:26 -0700

    Add dynamic routing functionality based on id parameter

--- src/app/sitter-detail/sitter-detail.component.ts
+++ src/app/sitter-detail/sitter-detail.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Sitter } from '../sitter.model';
 
 @Component({
   selector: 'app-sitter-detail',
   templateUrl: './sitter-detail.component.html',
   styleUrls: ['./sitter-detail.component.css']
 })
-export class SitterDetailComponent implements OnInit {
 
-  constructor() { }
+export class SitterDetailComponent implements OnInit {
+  sitterId: number = null;
+
+  constructor(private route: ActivatedRoute, private location: Location) {}
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.sitterId = parseInt(urlParameters['id']);
+    });
   }
 
 }
